959fea4a4812f8472223dc4ac1adcf891e2064f1
katierose26 <katierose.christopher26@trinityschoolnyc.org>
2026-05-10 15:56:52 -0400

    removed node stuff

--- server.py
+++ server.py
@@ -16,9 +16,6 @@ def page2():
 def page3():
     return render_template('page3.html')
 
-def home():
-    return "<h1>Welcome to My Website</h1>"
 
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+#if __name__ == '__main__':
+ #   app.run(debug=True, host='0.0.0.0', port=5001)
