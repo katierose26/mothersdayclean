261874bc5ce9d8ffee35a3bc096b414556e1c02a
katierose26 <katierose.christopher26@trinityschoolnyc.org>
2026-05-10 13:57:06 -0400

    Fresh clean upload

---
+++ server.py
@@ -0,0 +1,24 @@
+from flask import Flask
+import sqlite3
+from flask import Flask, render_template, request, url_for, flash, redirect, abort
+
+app = Flask(__name__)
+
+@app.route('/')
+def page1():
+    return render_template('page1.html')
+
+@app.route('/page2')
+def page2():
+    return render_template('page2.html')
+
+@app.route('/page3')
+def page3():
+    return render_template('page3.html')
+
+def home():
+    return "<h1>Welcome to My Website</h1>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5001)
